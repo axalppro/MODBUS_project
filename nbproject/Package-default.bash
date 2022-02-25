a7b782603515f222b333a35bf2b9d3eaf16de1aa
aurelien.rithner <aurelien.rithner@WEA30910.hevs.ch>
2022-02-25 13:20:32 +0100

    Revert "created branch working"
    This reverts commit fdfb6d2655591e46982ad6033d2c10cdb3aa34c0.

--- nbproject/Package-default.bash
+++ nbproject/Package-default.bash
@@ -10,9 +10,9 @@ CND_CONF=default
 CND_DISTDIR=dist
 TMPDIR=build/${CND_CONF}/${IMAGE_TYPE}/tmp-packaging
 TMPDIRNAME=tmp-packaging
-OUTPUT_PATH=dist/${CND_CONF}/${IMAGE_TYPE}/MODBUS_project.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
-OUTPUT_BASENAME=MODBUS_project.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
-PACKAGE_TOP_DIR=modbusproject/
+OUTPUT_PATH=dist/${CND_CONF}/${IMAGE_TYPE}/solar_soluce.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+OUTPUT_BASENAME=solar_soluce.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+PACKAGE_TOP_DIR=solarsoluce.x/
 
 # Functions
 function checkReturnCode
@@ -57,15 +57,15 @@ mkdir -p ${TMPDIR}
 
 # Copy files and create directories and links
 cd "${TOP}"
-makeDirectory ${TMPDIR}/modbusproject/bin
+makeDirectory ${TMPDIR}/solarsoluce.x/bin
 copyFileToTmpDir "${OUTPUT_PATH}" "${TMPDIR}/${PACKAGE_TOP_DIR}bin/${OUTPUT_BASENAME}" 0755
 
 
 # Generate tar file
 cd "${TOP}"
-rm -f ${CND_DISTDIR}/${CND_CONF}/package/modbusproject.tar
+rm -f ${CND_DISTDIR}/${CND_CONF}/package/solarsoluce.x.tar
 cd ${TMPDIR}
-tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/package/modbusproject.tar *
+tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/package/solarsoluce.x.tar *
 checkReturnCode
 
 # Cleanup
